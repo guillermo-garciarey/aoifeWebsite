dfe8ba90f07837308a3a55c170fc9e64a0d7868a
Guillermo Garcia <125451487+guillermo-garciarey@users.noreply.github.com>
2025-02-10 16:51:40 +0000

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,12 +3,12 @@ let songs = [
 		name: "Wonderwall",
 		path: "https://www.dropbox.com/scl/fi/5tvoyfd2g8hcex5jhii46/Think_of_What_Happened_to_Me_from_Fiver_Southwark_Playhouse_3_-_20_July.mp3?rlkey=75nj5p6e3b9fkrugjgfrwwscg&raw=1",
 		artist: "Aoife Clesham feat. Oasis",
-		cover: "../../images/disk.jpg",
+		cover: "../../images/disk.JPG",
 	},
 	{
 		name: "Live Forever",
 		path: "ttps://drive.google.com/uc?export=download&id=1XgsSyYDKYi02XLuNs15NR8KRJAQQdOL7",
 		artist: "Aoife Clesham feat. Oasis",
-		cover: "images/profile2.jpg",
+		cover: "images/profile2.JPG",
 	},
 ];
